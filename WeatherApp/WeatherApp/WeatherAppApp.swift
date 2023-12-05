8e2231a5205b1f376f2287dbaf8c21189472c9de
Kleogis <kleogis@hotmail.com>
2023-12-04 19:43:24 -0800

    create files for progect and added gradient

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Viktoriia Liu on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
